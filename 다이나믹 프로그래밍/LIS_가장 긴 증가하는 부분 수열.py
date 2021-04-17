d10f10dcbab3ddb4cdc42e9d950aa2724a299f41
Yegun Kim <wlfka102@gmail.com>
2021-04-18 01:37:01 +0900

    LIS 가장 긴 증가하는 부분 수열을 함수로 변경했습니다.

--- 다이나믹 프로그래밍/LIS_가장 긴 증가하는 부분 수열.py
+++ 다이나믹 프로그래밍/LIS_가장 긴 증가하는 부분 수열.py
@@ -4,22 +4,26 @@
 import sys
 rl = sys.stdin.readline
 
+
+def lis(n, a):
+    # 1로 DP 테이블 초기화
+    dp = [1] * n
+
+    for i in range(1, n):
+        # 0 부터 i - 1 까지 길이 갱신
+        for j in range(0, i):
+            # 모든 0 <= j < i 에 대하여,
+            if a[j] < a[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+
+    return dp
+
+
 # 'N' 배열 길이
 N = int(rl())
 # 'A' 배열
 A = list(map(int, rl().split()))
-
-# 1로 DP 테이블 초기화
-dp = [1] * N
-
-for i in range(1, N):
-    # 0 부터 i - 1 까지 길이 갱신
-    for j in range(0, i):
-        # 모든 0 <= j < i 에 대하여,
-        if A[j] < A[i]:
-            dp[i] = max(dp[i], dp[j] + 1)
-
-print(max(dp))
+print(max(lis(N, A)))
 
 '''
 [입력]
