0f748adf34b1ac4ca877eefee5ae74b1fead3c45
Yegun Kim <wlfka102@gmail.com>
2021-04-20 10:31:20 +0900

    Python 슬라이싱을 추가했습니다.

--- Matrix_행렬.py
+++ Matrix_행렬.py
@@ -4,5 +4,22 @@ def rotate90(m):
     """ 행렬 m 을 시계방향으로 90 도 회전시킨다. """
     return list(map(list, zip(*m[::-1])))
 
-
+a = [0, 1, 2, 3, 4, 5]
+start = 2
+stop = 4
+step = -1
+print(a[start:stop])    # [2, 3]                [start, stop)
+print(a[start:])        # [2, 3, 4, 5]          [start,
+print(a[:stop])         # [0, 1, 2, 3]          stop)
+print()
+print(a[:])             # [0, 1, 2, 3, 4, 5]    all
+print(a[-1])            # 5                     last
+print(a[-2:])           # [4, 5]                last 2
+print(a[:-2])           # [0, 1, 2, 3]          all - last 2
+print()
+print(a[::-1])          # [5, 4, 3, 2, 1, 0]    reverse
+print(a[1::-1])         # [1, 0]                first 2 reverse
+print(a[:-3:-1])        # [5, 4]                last 2 reverse
+print(a[-3::-1])        # [3, 2, 1, 0]          all - last 2 reverse
+print()
 print(rotate90([[1, 2], [3, 4]]))  # [[3, 1], [4, 2]]
