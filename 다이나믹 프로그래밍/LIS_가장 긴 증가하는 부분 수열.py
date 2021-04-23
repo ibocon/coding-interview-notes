3ad427f7c406c34edb8468237c64f498f4e1eee6
Yegun Kim <wlfka102@gmail.com>
2021-04-23 21:23:51 +0900

    LIS 변형 함수를 추가했다.

--- 다이나믹 프로그래밍/LIS_가장 긴 증가하는 부분 수열.py
+++ 다이나믹 프로그래밍/LIS_가장 긴 증가하는 부분 수열.py
@@ -1,10 +1,6 @@
 # DP(Dynamic Programming) 다이나믹 프로그래밍
 # LIS(Longest Increasing Subsequence) 가장 긴 증가하는 부분 수열
 
-import sys
-rl = sys.stdin.readline
-
-
 def lis(n, a):
     # 1로 DP 테이블 초기화
     dp = [1] * n
@@ -18,17 +14,24 @@ def lis(n, a):
 
     return dp
 
+N = 6
+A = [10, 20, 10, 30, 20, 50]
+print(max(lis(N, A)))  # 4
 
-# 'N' 배열 길이
-N = int(rl())
-# 'A' 배열
-A = list(map(int, rl().split()))
-print(max(lis(N, A)))
+def lnds(n, a):
+    """Longest Non-Decreasing Subsequence 가장 긴 감소하지 않는 부분 수열 """
+    # 1로 DP 테이블 초기화
+    dp = [1] * n
 
-'''
-[입력]
-6
-10 20 10 30 20 50
-[출력]
-4
-'''
+    for i in range(1, n):
+        # 0 부터 i - 1 까지 길이 갱신
+        for j in range(0, i):
+            # 모든 0 <= j < i 에 대하여,
+            if a[j] <= a[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+
+    return dp
+
+N = 6
+A = [10, 20, 10, 10, 20, 50]
+print(max(lnds(N, A)))  # 5
